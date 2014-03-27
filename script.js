3e5ce63e6aba5f1789b026bbabadab87c13712fc
Walter Michelin <wmichelin@gmail.com>
2014-03-27 16:20:51 -0400

    message of the day functionality
    pulls from DB, writes to DB

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-var numRows = 0;
+var numRows = 1;
+var counter = 0;
+var dataRef = new Firebase('https://sweltering-fire-9340.firebaseio.com/justforfun/myData');
 
 function hello()
 {
 	console.log("it works <3");
+	dataRef.on('value', function(snapshot){
+		if(snapshot.val() === null)
+		{
+			console.log("NULL BITCHES");
+		}
+		else
+		{
+			var x = snapshot.val();
+			console.log(x.stuff);
+			addToTable(x.stuff);
+		}
+	});
 }
 
 function execute(var1)
@@ -12,17 +26,34 @@ function execute(var1)
 	if(var1 != '')
 	{
 		console.log(var1);
+		//numRows = numRows + 1;
+		writeToDB(var1);
 		addToTable(var1);
+
 	}
 }
 
 function addToTable(var1)
 {	
 	var table = document.getElementById('table');
-	var row = table.insertRow(numRows + 1);
-	var cell1 = row.insertCell(0);
-	numRows = numRows + 1;
-	cell1.innerHTML = var1;
+	if(counter == 0)
+	{
+		var row = table.insertRow(numRows);
+		var cell = row.insertCell(0);
+		counter ++;
+	}
+	else
+	{
+		var cell = table.rows[1].cells.item(0);
+
+	}
+	cell.innerHTML = var1;
 
 }
+
+function writeToDB(var1)
+{
+	dataRef.child("stuff").set(var1);
+}
+
 
